d2b1e4838f8442fbe0d1328560e2752f16484e2e
Danilo Souza <47028834+danilosoftwares@users.noreply.github.com>
2024-07-19 20:18:46 -0300

    fix: added animation item dock

--- src/components/DockContainer/styles.ts
+++ src/components/DockContainer/styles.ts
@@ -1,11 +1,16 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const toHeight = keyframes`
+  from   { height: 63px }
+  to { height: 90px }
+`
 
 export const DockContainer = styled.div`
   position: absolute;
   left: 25%;
   height: 64px;
   width: max-content;
-  bottom: 0px;
+  bottom: 10px;
 
   display: flex;
   justify-content: center;
@@ -18,8 +23,12 @@ export const DockContainer = styled.div`
   border-radius: 16px;
 `
 export const ImageItemDock = styled.img`
+  height: 63px;
+ 
   :hover {
     height: 90px;
     margin-bottom: 30px;
+    animation: ${toHeight} 0.2s linear;
+    transition: height 0.2s linear;    
   }
 `
